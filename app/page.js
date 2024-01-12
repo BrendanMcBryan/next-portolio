d958d0c3f5e668ed2411bc5df36834d8106b5f50
brendan <brendan.mcbryan@gmail.com>
2024-01-12 15:31:19 -0800

    git testing

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='bg-sky-500 h-screen'>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline text-center">
       Hello world!
         </h1>
     </main>
