7ff95449e4a250f298e0f8d6ae9bff7dfd1076bd
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-13 13:41:32 -0800

    react icon is working

--- app/page.js
+++ app/page.js
@@ -1,11 +1,31 @@
-import Image from 'next/image'
-
+import Head from "next/head";
+import { BsMoonStarsFill } from "react-icons/bs";
 export default function Home() {
   return (
-    <main className='bg-sky-500 h-screen'>
-      <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-        </h1>
-    </main>
-  )
+    <div>
+      <Head>
+        <title>brendan mcbryan portfolio</title>
+        <meta name="description" content="brendans portfolio"></meta>
+        <link rel="icon" href="=/favicon.ico" />
+      </Head>
+      <main className="bg-cyan-400 h-screen">
+        <section className="bg-blue-700 ">
+          <nav>
+            <h1> developed by brendan</h1>
+            <ul>
+              <li><BsMoonStarsFill /></li>
+              <li>
+                <a href="https://www.linkedin.com/in/brendan-mcbryan-b5b543b/">
+                  linkedin
+                </a>
+              </li>
+              <li>
+                <a href="#">resume</a>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </main>
+    </div>
+  );
 }
